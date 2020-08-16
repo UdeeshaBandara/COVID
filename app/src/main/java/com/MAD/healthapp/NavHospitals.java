3d5d4cc6e42302c00648bd39eed76ae07143d93d
Udeesha Bandara <udeeshabandara@gmail.com>
2020-08-16 14:39:30 +0530

    New Navigation

--- app/src/main/java/com/MAD/healthapp/NavHospitals.java
+++ app/src/main/java/com/MAD/healthapp/NavHospitals.java
@@ -7,11 +7,24 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.LocationListener;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -31,25 +44,28 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-public class NavHospitals extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, LocationListener {
+public class NavHospitals extends FragmentActivity implements OnMapReadyCallback {
 
+    private String calculatedDestination="";
+    LatLng current_position;
     private GoogleMap mMap;
-    GoogleApiClient mGoogleApiClient;
-
-    double currentLatitude,currentLongitude;
-    Location myLocation;
-
-    private static final int REQUEST_CHECK_SETTING_GPS=0x1;
-    private static final int REQUEST_ID_MULTIPLE_PERMISSIONS=0x2;
-
-
-
-
+    private static final int LOCATION_REQUEST = 500;
+    ArrayList<LatLng> listPoints;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,197 +74,228 @@ public class NavHospitals extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        setUPGClient();
+        listPoints = new ArrayList<>();
     }
 
-    private void setUPGClient() {
-        mGoogleApiClient= new GoogleApiClient.Builder(this)
-                .enableAutoManage(this,0,this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-        mGoogleApiClient.connect();
-
-    }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST);
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                //Reset marker when already 2
+                if (listPoints.size() == 1) {
+                    listPoints.clear();
+                    mMap.clear();
+                }
+                //Save first point select
+                listPoints.add(latLng);
+                //Create marker
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        checkPermission();
-    }
+                //Add first marker to the map
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                mMap.addMarker(markerOptions);
+                current_position=listPoints.get(0);
+                searchNearBy();
 
-    private void checkPermission() {
-        int permissionLocation= ContextCompat.checkSelfPermission(NavHospitals.this,
-                Manifest.permission.ACCESS_FINE_LOCATION);
 
-        List<String> listpermission= new ArrayList<>();
-        if (permissionLocation != PackageManager.PERMISSION_GRANTED){
-            listpermission.add(Manifest.permission.ACCESS_FINE_LOCATION);
-
-            if(!listpermission.isEmpty()){
-                ActivityCompat.requestPermissions(this,
-                        listpermission.toArray(new String[listpermission.size()]),REQUEST_ID_MULTIPLE_PERMISSIONS);
 
             }
-        }else {
-            getMyLocation();
-        }
+        });
 
     }
 
+    private void searchNearBy()
+    {
+        String mLatLong = current_position.latitude + "," + current_position.longitude;
+        String url="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+mLatLong+"&radius=5000&types=hospital&key=AIzaSyAPrTPADT_tYmMJYjg6nZZ4jUHLJILoWpM";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+
+                    JSONObject hospital = new JSONObject(response);
+                    double lat=Double.parseDouble(hospital.getJSONArray("results").getJSONObject(0).getJSONObject("geometry").getJSONObject("location").getString("lat"));
+                    double lng=Double.parseDouble(hospital.getJSONArray("results").getJSONObject(0).getJSONObject("geometry").getJSONObject("location").getString("lng"));
+                    Log.e("lat",String.valueOf(lat));
+                    LatLng location = new LatLng(lat, lng);
+                    String url = getRequestUrl(location, listPoints.get(0));
+
+                    TaskRequestDirections taskRequestDirections = new TaskRequestDirections();
+                    taskRequestDirections.execute(url);
+
+
+                    //ArrayAdapter<String> Adaptor = new ArrayAdapter<String>(v.getContext(), android.R.layout.simple_list_item_1, countries[0]);
+                    //Adaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    // txt_countries.setAdapter(Adaptor);
+
+
+                } catch (Exception ex) {
+
+                    Log.e("Response", ex.getMessage());
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(request);
+
+    }
+
+    private String getRequestUrl(LatLng origin, LatLng dest) {
+        //Value of origin
+        String str_org = "origin=" + origin.latitude +","+origin.longitude;
+        //Value of destination
+        String str_dest = "destination=" + dest.latitude+","+dest.longitude;
+        //Set value enable the sensor
+        String sensor = "sensor=false";
+        //Mode for find direction
+        String mode = "mode=driving";
+        //Build the full param
+        String key="key=AIzaSyAPrTPADT_tYmMJYjg6nZZ4jUHLJILoWpM";
+        String param = str_org +"&" + str_dest + "&" +sensor+"&" +mode+"&"+key;
+        //Output format
+        String output = "json";
+        //Create url to request
+
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + param;
+        return url;
+    }
+
+    private String requestDirection(String reqUrl) throws IOException {
+        String responseString = "";
+        InputStream inputStream = null;
+        HttpURLConnection httpURLConnection = null;
+        try{
+            URL url = new URL(reqUrl);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.connect();
+
+            //Get the response result
+            inputStream = httpURLConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            StringBuffer stringBuffer = new StringBuffer();
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuffer.append(line);
+            }
+
+            responseString = stringBuffer.toString();
+            bufferedReader.close();
+            inputStreamReader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            httpURLConnection.disconnect();
+        }
+        return responseString;
+    }
+
+    @SuppressLint("MissingPermission")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        int permissionLocation=ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION);
-
-        if (permissionLocation==PackageManager.PERMISSION_GRANTED){
-
-            getMyLocation();
-
-        }else {
-            checkPermission();
-        }
-    }
-
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-        myLocation=location;
-        if(myLocation!=null){
-            currentLatitude=location.getLatitude();
-            currentLongitude=location.getLongitude();
-
-
-            BitmapDescriptor icon=  BitmapDescriptorFactory.fromResource(R.drawable.navigation);
-
-
-            // Add a marker in Sydney and move the camera
-            mMap.clear();
-            LatLng me = new LatLng(currentLatitude, currentLongitude);
-            mMap.addMarker(new MarkerOptions().position(me).title("Me"));
-
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(me));
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLatitude,currentLongitude), 15.0f));
-
-
-            getNearByHospital();
-        }
-
-
-
-    }
-
-
-
-    private void getNearByHospital() {
-
-        StringBuilder stringBuilder= new StringBuilder("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?");
-        stringBuilder.append("location="+String.valueOf(currentLatitude)+","+String.valueOf(currentLongitude)) ;
-
-        stringBuilder.append("&radius=10000");
-        stringBuilder.append("&type=hospital");
-        stringBuilder.append("&key="+getResources().getString(R.string.google_maps_key));
-        String url=stringBuilder.toString();
-        Object dataTransfer[]=new Object[2];
-        dataTransfer[0]=mMap;
-        dataTransfer[1]=url;
-
-        NearPlaces nearPlaces=new NearPlaces();
-        nearPlaces.execute(dataTransfer);
-    }
-
-    private void getMyLocation(){
-        if(mGoogleApiClient!=null) {
-            if (mGoogleApiClient.isConnected()) {
-                int permissionLocation = ContextCompat.checkSelfPermission(NavHospitals.this,
-                        Manifest.permission.ACCESS_FINE_LOCATION);
-                if (permissionLocation == PackageManager.PERMISSION_GRANTED) {
-                    myLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-                    LocationRequest locationRequest = new LocationRequest();
-                    locationRequest.setInterval(3000);
-                    locationRequest.setFastestInterval(3000);
-                    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-                    LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                            .addLocationRequest(locationRequest);
-                    builder.setAlwaysShow(true);
-                    LocationServices.FusedLocationApi
-                            .requestLocationUpdates(mGoogleApiClient, locationRequest, (com.google.android.gms.location.LocationListener) this);
-                    PendingResult<LocationSettingsResult> result =
-                            LocationServices.SettingsApi
-                                    .checkLocationSettings(mGoogleApiClient, builder.build());
-                    result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
-
-                        @Override
-                        public void onResult(LocationSettingsResult result) {
-                            final Status status = result.getStatus();
-                            switch (status.getStatusCode()) {
-                                case LocationSettingsStatusCodes.SUCCESS:
-                                    // All location settings are satisfied.
-                                    // You can initialize location requests here.
-                                    int permissionLocation = ContextCompat
-                                            .checkSelfPermission(NavHospitals.this,
-                                                    Manifest.permission.ACCESS_FINE_LOCATION);
-                                    if (permissionLocation == PackageManager.PERMISSION_GRANTED) {
-
-
-                                        myLocation = LocationServices.FusedLocationApi
-                                                .getLastLocation(mGoogleApiClient);
-
-
-                                    }
-                                    break;
-                                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                                    // Location settings are not satisfied.
-                                    // But could be fixed by showing the user a dialog.
-                                    try {
-                                        // Show the dialog by calling startResolutionForResult(),
-                                        // and check the result in onActivityResult().
-                                        // Ask to turn on GPS automatically
-                                        status.startResolutionForResult(NavHospitals.this,
-                                                REQUEST_CHECK_SETTING_GPS);
-
-
-                                    } catch (IntentSender.SendIntentException e) {
-                                        // Ignore the error.
-                                    }
-
-
-                                    break;
-                                case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                                    // Location settings are not satisfied.
-                                    // However, we have no way
-                                    // to fix the
-                                    // settings so we won't show the dialog.
-                                    // finish();
-                                    break;
-                            }
-                        }
-                    });
-
+        switch (requestCode){
+            case LOCATION_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mMap.setMyLocationEnabled(true);
                 }
+                break;
+        }
+    }
+
+    public class TaskRequestDirections extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String responseString = "";
+            try {
+                responseString = requestDirection(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+            return  responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            //Parse json here
+            TaskParser taskParser = new TaskParser();
+            taskParser.execute(s);
+        }
+    }
+
+    public class TaskParser extends AsyncTask<String, Void, List<List<HashMap<String, String>>> > {
+
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... strings) {
+            JSONObject jsonObject = null;
+            List<List<HashMap<String, String>>> routes = null;
+            try {
+                jsonObject = new JSONObject(strings[0]);
+                DirectionsParser directionsParser = new DirectionsParser();
+                routes = directionsParser.parse(jsonObject);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> lists) {
+            //Get list route and display it into the map
+
+            ArrayList points = null;
+
+            PolylineOptions polylineOptions = null;
+
+            for (List<HashMap<String, String>> path : lists) {
+                points = new ArrayList();
+                polylineOptions = new PolylineOptions();
+
+                for (HashMap<String, String> point : path) {
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lon = Double.parseDouble(point.get("lon"));
+
+                    points.add(new LatLng(lat,lon));
+                }
+
+                polylineOptions.addAll(points);
+                polylineOptions.width(15);
+                polylineOptions.color(Color.BLUE);
+                polylineOptions.geodesic(true);
+            }
+
+            if (polylineOptions!=null) {
+                mMap.addPolyline(polylineOptions);
+            } else {
+                Toast.makeText(getApplicationContext(), "Direction not found!", Toast.LENGTH_SHORT).show();
+            }
+
         }
     }
 }
